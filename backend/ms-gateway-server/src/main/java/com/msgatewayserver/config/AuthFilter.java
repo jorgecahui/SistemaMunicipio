a3fe3a88f6f31a48a28d15c1232d8f6c678ebdf6
jorgecahui <jorgecahui347@gmail.com>
2025-10-17 21:31:39 -0500

    sd

--- backend/ms-gateway-server/src/main/java/com/msgatewayserver/config/AuthFilter.java
+++ backend/ms-gateway-server/src/main/java/com/msgatewayserver/config/AuthFilter.java
@@ -4,17 +4,14 @@ import com.msgatewayserver.dto.TokenDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
 
 @Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
@@ -30,15 +27,15 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            String path = exchange.getRequest().getURI().getPath();
 
-            if (path.contains("/auth")) {
-                return chain.filter(exchange);
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, HttpStatus.BAD_REQUEST);
             }
 
             String tokenHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
             if (tokenHeader == null || !tokenHeader.startsWith("Bearer ")) {
-                return onError(exchange, HttpStatus.UNAUTHORIZED, "Falta o es inválido el token de autorización");
+                return onError(exchange, HttpStatus.BAD_REQUEST);
             }
 
             String token = tokenHeader.substring(7);
@@ -50,17 +47,14 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
                     .bodyToMono(TokenDto.class)
                     .map(response -> exchange)
                     .flatMap(chain::filter)
-                    .onErrorResume(e -> onError(exchange, HttpStatus.UNAUTHORIZED, "Token inválido o expirado"));
+                    .onErrorResume(e -> onError(exchange, HttpStatus.UNAUTHORIZED));
         };
     }
 
-    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status, String message) {
+    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(status);
-        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-        byte[] bytes = ("{\"error\":\"" + message + "\"}").getBytes(StandardCharsets.UTF_8);
-        DataBuffer buffer = response.bufferFactory().wrap(bytes);
-        return response.writeWith(Mono.just(buffer));
+        return response.setComplete();
     }
 
     public static class Config {}
